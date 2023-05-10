c767b76e1acd4e374acacd9eb76273cfb194cb19
siegerts <stephen.siegert@gmail.com>
2023-05-10 11:05:14 -0400

    again

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -30,25 +30,6 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             >
               Vercel
             </a>
-            . Illustrations by{" "}
-            <a
-              href="https://popsy.co"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Popsy
-            </a>
-            . The source code is available on{" "}
-            <a
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              GitHub
-            </a>
-            .
           </p>
         </div>
         <ModeToggle />
