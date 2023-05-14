cf57862b832d9af51ae713024228f2f7b0988125
siegerts <stephen.siegert@gmail.com>
2023-05-13 22:56:16 -0400

    try jwt again

--- lib/auth.ts
+++ lib/auth.ts
@@ -15,9 +15,9 @@ export const authOptions: NextAuthOptions = {
   // This is a temporary fix for prisma client.
   // @see https://github.com/prisma/prisma/issues/16117
   adapter: PrismaAdapter(db as any),
-  // session: {
-  //   strategy: "jwt",
-  // },
+  session: {
+    strategy: "jwt",
+  },
   pages: {
     signIn: "/login",
   },
@@ -77,7 +77,16 @@ export const authOptions: NextAuthOptions = {
         session.user.username = user.username;
         return session;
       },
-    
+      
+      async jwt({ token, user }) {
+        if (user) {
+          // @ts-ignore
+          token.id = user.id;
+          // @ts-ignore
+          token.username = user.username;
+        }
+        return token;
+      }
 
     
   },
