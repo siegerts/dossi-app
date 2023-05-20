b51083aa3397fc375e3d76305aebf19e278ae702
siegerts <stephen.siegert@gmail.com>
2023-05-20 12:31:19 -0400

    add in pin apis

---
+++ app/api/pins/[pinId]/route.ts
@@ -0,0 +1,53 @@
+import { getServerSession } from "next-auth"
+import * as z from "zod"
+
+import { authOptions } from "@/lib/auth"
+import prisma from "@/lib/prisma"
+
+const routeContextSchema = z.object({
+  params: z.object({
+    pinId: z.string(),
+  }),
+})
+
+export async function DELETE(
+  req: Request,
+  context: z.infer<typeof routeContextSchema>
+) {
+  try {
+    // Validate the route params.
+    const { params } = routeContextSchema.parse(context)
+
+    // Check if the user has access to this note.
+    if (!(await verifyCurrentUserHasAccessToPin(params.pinId))) {
+      return new Response(null, { status: 403 })
+    }
+
+    // Delete the note.
+    await prisma.pin.delete({
+      where: {
+        id: params.pinId as string,
+      },
+    })
+
+    return new Response(null, { status: 204 })
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 })
+    }
+
+    return new Response(null, { status: 500 })
+  }
+}
+
+async function verifyCurrentUserHasAccessToPin(pinId: string) {
+  const session = await getServerSession(authOptions)
+  const count = await prisma.pin.count({
+    where: {
+      id: pinId,
+      authorId: session?.user.id,
+    },
+  })
+
+  return count > 0
+}
---
+++ app/api/pins/route.ts
@@ -0,0 +1,83 @@
+import { getServerSession } from "next-auth/next"
+import * as z from "zod"
+
+import { authOptions } from "@/lib/auth"
+import { RequiresProPlanError } from "@/lib/exceptions"
+import prisma from "@/lib/prisma"
+
+const pinCreateSchema = z.object({
+  url: z.string().url({ message: "Invalid url" }),
+})
+
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+
+    const { user } = session
+
+    const pins = await prisma.pin.findMany({
+      where: {
+        authorId: user.id,
+      },
+    })
+
+    return new Response(JSON.stringify(pins), { status: 200 })
+  } catch (error) {
+    return new Response(null, { status: 500 })
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+
+    const { user } = session
+
+    // const subscriptionPlan = await getUserSubscriptionPlan(user.id)
+
+    // If user is on a free plan.
+    // Check if user has reached limit of 3 posts.
+    // if (!subscriptionPlan?.isPro) {
+    //   const count = await prisma.post.count({
+    //     where: {
+    //       authorId: user.id,
+    //     },
+    //   })
+
+    //   if (count >= 3) {
+    //     throw new RequiresProPlanError()
+    //   }
+    // }
+
+    const json = await req.json()
+
+    const { url } = pinCreateSchema.parse(json)
+
+    const pin = await prisma.pin.create({
+      data: {
+        url,
+        authorId: user.id,
+      },
+    })
+
+    return new Response(JSON.stringify(pin), { status: 201 })
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 })
+    }
+
+    if (error instanceof RequiresProPlanError) {
+      return new Response("Requires Pro Plan", { status: 402 })
+    }
+
+    return new Response(null, { status: 500 })
+  }
+}
