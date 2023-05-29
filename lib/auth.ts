cea749bf312a219735760f4f43be8ea36de9398c
siegerts <stephen.siegert@gmail.com>
2023-05-29 19:28:16 -0400

    try jwt + db strategy

--- lib/auth.ts
+++ lib/auth.ts
@@ -65,15 +65,49 @@ export const authOptions: NextAuthOptions = {
     //   },
     // }),
   ],
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
-    async session({ session, user }) {
-      // @ts-ignore
-      session.user.id = user.id
-      // @ts-ignore
-      session.user.username = user.username
-      // @ts-ignore
-      session.user.role = user.role
+    // async session({ session, user }) {
+    //   // @ts-ignore
+    //   session.user.id = user.id
+    //   // @ts-ignore
+    //   session.user.username = user.username
+    //   // @ts-ignore
+    //   session.user.role = user.role
+    //   return session
+    // },
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
       return session
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.id,
+        },
+      })
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
     },
   },
 }
