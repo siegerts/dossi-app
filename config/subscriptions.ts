b69083a5e046e662b835eb7e3b16fbece3d12635
siegerts <stephen.siegert@gmail.com>
2023-06-02 17:59:32 -0400

    subscription utils

---
+++ config/subscriptions.ts
@@ -0,0 +1,16 @@
+import { env } from "@/env.mjs"
+
+import { SubscriptionPlan } from "types"
+
+export const freePlan: SubscriptionPlan = {
+  name: "Free",
+  description:
+    "The free plan is limited to 3 posts. Upgrade to the PRO plan for unlimited posts.",
+  stripePriceId: "",
+}
+
+export const proPlan: SubscriptionPlan = {
+  name: "PRO",
+  description: "The PRO plan has unlimited posts.",
+  stripePriceId: env.STRIPE_PRO_MONTHLY_PLAN_ID || "",
+}
