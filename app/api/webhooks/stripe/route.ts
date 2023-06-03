96712c9931c4782796e2e402de578050bccab87e
siegerts <stephen.siegert@gmail.com>
2023-06-02 20:05:43 -0400

    stripe webhook

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,88 @@
+import { headers } from "next/headers"
+import { env } from "@/env.mjs"
+import Stripe from "stripe"
+
+import prisma from "@/lib/prisma"
+import { stripe } from "@/lib/stripe"
+
+export async function POST(req: Request) {
+  const body = await req.text()
+  const signature = headers().get("Stripe-Signature") as string
+
+  let event: Stripe.Event
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      env.STRIPE_WEBHOOK_SECRET
+    )
+  } catch (error: any) {
+    return new Response(`Webhook Error: ${error?.message}`, {
+      status: 400,
+    })
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session
+
+  if (event.type === "checkout.session.completed") {
+    // Retrieve the subscription details from Stripe.
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    )
+
+    // Update the user stripe into in our database.
+    // Since this is the initial subscription, we need to update
+    // the subscription id and customer id.
+    await prisma.user.update({
+      where: {
+        id: session?.metadata?.userId,
+      },
+      data: {
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer as string,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    })
+  }
+
+  if (event.type === "invoice.payment_succeeded") {
+    // Retrieve the subscription details from Stripe.
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    )
+
+    // Update the price id and set the new period end.
+    await prisma.user.update({
+      where: {
+        stripeSubscriptionId: subscription.id,
+      },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    })
+  }
+
+  if (event.type === "customer.deleted") {
+    // Update the user in our database to remove the subscription id and customer id.
+    await prisma.user.update({
+      where: {
+        stripeCustomerId: session.id,
+      },
+      data: {
+        stripeSubscriptionId: null,
+        stripeCustomerId: null,
+        stripePriceId: null,
+        stripeCurrentPeriodEnd: null,
+      },
+    })
+  }
+
+  return new Response(null, { status: 200 })
+}
