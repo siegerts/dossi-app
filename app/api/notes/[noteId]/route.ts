853f8d3f3e2e64acf38818119c1d7fb3b520373b
siegerts <stephen.siegert@gmail.com>
2023-06-04 14:21:26 -0400

    use schema connections/relations to reduce db queries

--- app/api/notes/[noteId]/route.ts
+++ app/api/notes/[noteId]/route.ts
@@ -48,18 +48,24 @@ export async function DELETE(
   context: z.infer<typeof routeContextSchema>
 ) {
   try {
-    // Validate the route params.
-    const { params } = routeContextSchema.parse(context)
+    const session = await getServerSession(authOptions)
 
-    // Check if the user has access to this note.
-    if (!(await verifyCurrentUserHasAccessToNote(params.noteId))) {
-      return new Response(null, { status: 403 })
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 })
     }
 
-    // Delete the note.
-    await prisma.note.delete({
-      where: {
-        id: params.noteId as string,
+    const { params } = routeContextSchema.parse(context)
+
+    await prisma.user.update({
+      where: { id: session?.user.id },
+      data: {
+        notes: {
+          delete: [
+            {
+              id: params.noteId,
+            },
+          ],
+        },
       },
     })
 
@@ -77,28 +83,33 @@ export async function PATCH(
   req: Request,
   context: z.infer<typeof routeContextSchema>
 ) {
-  // check for session sooner
   try {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+
     // Validate route params.
     const { params } = routeContextSchema.parse(context)
-
-    // Check if the user has access to this note.
-    if (!(await verifyCurrentUserHasAccessToNote(params.noteId))) {
-      return new Response(null, { status: 403 })
-    }
 
     // Get the request body and validate it.
     const json = await req.json()
     const body = notePatchSchema.parse(json)
 
-    // Update the note.
-    // TODO: Implement sanitization for content.
-    await prisma.note.update({
-      where: {
-        id: params.noteId,
-      },
+    await prisma.user.update({
+      where: { id: session?.user.id },
       data: {
-        content: body.content,
+        notes: {
+          update: {
+            where: {
+              id: params.noteId,
+            },
+            data: {
+              content: body.content,
+            },
+          },
+        },
       },
     })
 
@@ -110,16 +121,4 @@ export async function PATCH(
 
     return new Response(null, { status: 500 })
   }
-}
-
-async function verifyCurrentUserHasAccessToNote(noteId: string) {
-  const session = await getServerSession(authOptions)
-  const count = await prisma.note.count({
-    where: {
-      id: noteId,
-      userId: session?.user.id,
-    },
-  })
-
-  return count > 0
 }
