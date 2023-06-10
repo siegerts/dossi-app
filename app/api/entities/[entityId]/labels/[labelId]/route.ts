53461fa6daf89d2ee5a3eae7e035d5c1bc3cfc9e
siegerts <stephen.siegert@gmail.com>
2023-06-10 19:25:23 -0400

    api route for connect label and entity

---
+++ app/api/entities/[entityId]/labels/[labelId]/route.ts
@@ -0,0 +1,79 @@
+import { create } from "domain"
+import { getServerSession } from "next-auth"
+import * as z from "zod"
+
+import { authOptions } from "@/lib/auth"
+import prisma from "@/lib/prisma"
+
+const routeContextSchema = z.object({
+  params: z.object({
+    entityId: z.string(),
+    labelId: z.string(),
+  }),
+})
+
+export async function PUT(
+  req: Request,
+  context: z.infer<typeof routeContextSchema>
+) {
+  try {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+
+    const { user } = session
+    const { params } = routeContextSchema.parse(context)
+
+    await prisma.labelsOnEntities.create({
+      data: {
+        labelId: params.labelId,
+        entityId: params.entityId,
+        userId: user.id,
+      },
+    })
+
+    return new Response(null, { status: 201 })
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 })
+    }
+
+    return new Response(null, { status: 500 })
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  context: z.infer<typeof routeContextSchema>
+) {
+  try {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+
+    // Validate route params.
+    const { params } = routeContextSchema.parse(context)
+
+    await prisma.labelsOnEntities.delete({
+      where: {
+        userId_entityId_labelId: {
+          userId: session.user.id,
+          entityId: params.entityId,
+          labelId: params.labelId,
+        },
+      },
+    })
+
+    return new Response(null, { status: 204 })
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 })
+    }
+
+    return new Response(null, { status: 500 })
+  }
+}
