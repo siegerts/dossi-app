374aedcebcd9f2040d7bf98776a8917bd976f248
siegerts <stephen.siegert@gmail.com>
2023-06-25 15:10:15 -0400

    add activity route

---
+++ app/api/activity/route.ts
@@ -0,0 +1,42 @@
+import { getServerSession } from "next-auth/next"
+
+import { authOptions } from "@/lib/auth"
+import prisma from "@/lib/prisma"
+
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+
+    const { user } = session
+
+    const thirtyDaysAgo = new Date()
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30)
+
+    let activity = await prisma.note.findMany({
+      where: {
+        createdAt: {
+          gte: thirtyDaysAgo,
+        },
+        userId: user.id,
+      },
+      select: {
+        id: true,
+        createdAt: true,
+        title: true,
+        url: true,
+      },
+      take: 10,
+      orderBy: {
+        createdAt: "desc",
+      },
+    })
+
+    return new Response(JSON.stringify(activity), { status: 200 })
+  } catch (error) {
+    return new Response(null, { status: 500 })
+  }
+}
