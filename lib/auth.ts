dce6f0112ad13d6e88b4eb7480affb1f65ec363b
siegerts <stephen.siegert@gmail.com>
2023-06-28 15:59:44 -0400

    fix auth?

--- lib/auth.ts
+++ lib/auth.ts
@@ -44,7 +44,7 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       const dbUser = await prisma.user.findFirst({
         where: {
-          email: token.id,
+          email: token.email,
         },
       })
 
