b440581617ad424c7fdaf84a716cc0086581aa17
siegerts <stephen.siegert@gmail.com>
2023-07-27 12:22:54 -0400

    json download

--- app/(dashboard)/dashboard/components/data-table-toolbar.tsx
+++ app/(dashboard)/dashboard/components/data-table-toolbar.tsx
@@ -7,6 +7,12 @@ import { Table } from "@tanstack/react-table"
 import { Parser } from "json2csv"
 
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 
 import { DataTableFacetedFilter } from "./data-table-faceted-filter"
@@ -30,7 +36,7 @@ export function DataTableToolbar<TData>({
     id: label.id,
   }))
 
-  const downloadSelectedRows = () => {
+  const downloadSelectedRowsAsCSV = () => {
     const json2csvParser = new Parser()
     const csv = json2csvParser.parse(
       table.getSelectedRowModel().flatRows.map((row) => row.original)
@@ -44,6 +50,86 @@ export function DataTableToolbar<TData>({
     hiddenElement.download = "download.csv"
     hiddenElement.click()
   }
+
+  const downloadSelectedRowsAsJSON = () => {
+    const jsonData = JSON.stringify(
+      table.getSelectedRowModel().flatRows.map((row) => row.original),
+      null,
+      2
+    )
+    const jsonBlob = new Blob([jsonData], { type: "application/json" })
+    const jsonUrl = URL.createObjectURL(jsonBlob)
+
+    let hiddenElement = document.createElement("a")
+    hiddenElement.href = jsonUrl
+    hiddenElement.target = "_blank"
+    hiddenElement.download = "download.json"
+    hiddenElement.click()
+  }
+
+  // const downloadSelectedRowsAsMarkdown = () => {
+  //   let markdownData = "# My Data\n\n"
+  //   const columns = table.getAllColumns().map((column) => column.id)
+
+  //   table.getSelectedRowModel().flatRows.forEach((row, index) => {
+  //     const data = row.original
+  //     markdownData += `## Item ${index + 1}\n\n` // Header for each item
+
+  //     console.log(data)
+
+  //     for (let key in data) {
+  //       if (columns.includes(key)) {
+  //         markdownData += `### ${key}\n`
+
+  //         if (Array.isArray(data[key])) {
+  //           // @ts-ignore
+  //           const convertToMarkdown = (item, indentation = "") => {
+  //             let markdown = ""
+  //             if (Array.isArray(item)) {
+  //               item.forEach((arrayItem, index) => {
+  //                 markdown += `${indentation}- Item ${
+  //                   index + 1
+  //                 }:\n${convertToMarkdown(arrayItem, indentation + "  ")}\n`
+  //               })
+  //             } else if (typeof item === "object" && item !== null) {
+  //               for (let [key, value] of Object.entries(item)) {
+  //                 markdown += `${indentation}- ${key}:\n${convertToMarkdown(
+  //                   value,
+  //                   indentation + "  "
+  //                 )}\n`
+  //               }
+  //             } else {
+  //               markdown += `${indentation}- ${item}\n`
+  //             }
+  //             return markdown
+  //           }
+  //           // @ts-ignore
+  //           data[key].forEach((item, itemIndex) => {
+  //             markdownData += `  ${itemIndex + 1}.:\n${convertToMarkdown(
+  //               item,
+  //               "  "
+  //             )}`
+  //           })
+  //         } else if (typeof data[key] === "object" && data[key] !== null) {
+  //           for (let subKey in data[key]) {
+  //             markdownData += `  ${subKey}: ${data[key][subKey]}\n`
+  //           }
+  //         } else {
+  //           markdownData += `${data[key]}\n\n`
+  //         }
+  //       }
+  //     }
+  //   })
+
+  //   const markdownBlob = new Blob([markdownData], { type: "text/markdown" })
+  //   const markdownUrl = URL.createObjectURL(markdownBlob)
+
+  //   let hiddenElement = document.createElement("a")
+  //   hiddenElement.href = markdownUrl
+  //   hiddenElement.target = "_blank"
+  //   hiddenElement.download = "download.md"
+  //   hiddenElement.click()
+  // }
 
   return (
     <div className="flex items-center justify-between">
@@ -81,13 +167,27 @@ export function DataTableToolbar<TData>({
         )}
       </div>
       {table.getSelectedRowModel().flatRows.length > 0 && (
-        <Button
-          variant="ghost"
-          onClick={() => downloadSelectedRows()}
-          className="mr-2 h-8 px-2 lg:px-3">
-          Download
-          <DownloadIcon className="ml-2 h-4 w-4" />
-        </Button>
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="mr-2 h-8 px-2 lg:px-3">
+                Download <DownloadIcon className="ml-2 h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-25">
+              <DropdownMenuItem onClick={() => downloadSelectedRowsAsCSV()}>
+                CSV (.csv)
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => downloadSelectedRowsAsJSON()}>
+                JSON (.json)
+              </DropdownMenuItem>
+              {/* <DropdownMenuItem
+                onClick={() => downloadSelectedRowsAsMarkdown()}>
+                Markdown
+              </DropdownMenuItem> */}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </>
       )}
       <DataTableViewOptions table={table} />
     </div>
