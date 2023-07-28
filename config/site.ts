c6f1868b052f1a3860cf4e6d114cbfdbd7015270
siegerts <stephen.siegert@gmail.com>
2023-07-28 10:30:35 -0400

    site description

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "types"
 export const siteConfig: SiteConfig = {
   name: "dossi",
   description:
-    "Your own private GitHub notes on every page. Privates notes and labels across all repos, issues, PRs, and discussions. Remember your ideas and thoughts in context, right where you need them.",
+    "Your own private GitHub notes on every page. Private notes and labels across all repos, issues, PRs, and discussions. Use the browser extension to remember your ideas and thoughts in context, right where you need them.",
   url: "https://www.dossi.dev",
   ogImage: "https://www.dossi.dev/og.png",
   mainNav: [
