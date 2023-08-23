b8469af38e3634dbbfe1fb3cde4b9e2ea394d9b7
siegerts <stephen.siegert@gmail.com>
2023-08-23 13:31:26 -0400

    update datatable image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -87,28 +87,50 @@ export default async function IndexPage() {
           </div>
         </div>
       </section>
-      <section id="video" className="w-100 mx-auto px-4 py-8 md:w-5/6 lg:w-3/4">
-        <div
-          className="video overflow-hidden rounded-lg shadow-xl"
-          style={{
-            position: "relative",
-            paddingBottom: "56.25%" /* 16:9 */,
-            paddingTop: 25,
-            height: 0,
-          }}>
-          <iframe
+      <section
+        id="video"
+        className="w-100 spacing-lg mx-auto px-4 py-8 md:w-5/6 lg:w-3/4">
+        <h2 className="my-10 scroll-m-20 pb-1 text-3xl font-semibold tracking-tight first:mt-0">
+          Capture thoughts in the moment with the extension.
+        </h2>
+        <div className="mb-10">
+          <div
+            className="video overflow-hidden rounded-lg shadow-xl"
             style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-            }}
-            src={`https://www.youtube.com/embed/wgGGjAqa3L8`}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          />
+              position: "relative",
+              paddingBottom: "56.25%" /* 16:9 */,
+              paddingTop: 25,
+              height: 0,
+            }}>
+            <iframe
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+              }}
+              src={`https://www.youtube.com/embed/wgGGjAqa3L8`}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <h2 className="mt-10 scroll-m-20 pb-1 text-3xl font-semibold tracking-tight first:mt-0">
+            Use the web app to manage notes, pins, and labels.
+          </h2>
+          <div className="overflow-hidden rounded-lg shadow-xl">
+            <Image
+              src="/images/dossi-dashboard.png"
+              width={1000}
+              height={600}
+              alt="dossi popup"
+              className="mt-3 rounded-md"
+            />
+          </div>
         </div>
       </section>
     </>
