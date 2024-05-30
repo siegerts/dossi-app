aa2417c60fa6b5db1c7f475f2feb1307dd6402fb
siegerts <stephen.siegert@gmail.com>
2024-05-30 11:48:16 -0400

    migrate to neon + update next

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { withContentlayer } = require("next-contentlayer")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com"],
